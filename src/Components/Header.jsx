f9a4a895021e5a6bb03ceaad65424bd2fd11a7d5
spardutti <luisdamian.sp@gmail.com>
2021-01-24 16:25:34 -0300

    added some querys

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 
-
-const Header = props => {
-
-    return(
-        <nav className="navContainer">
-        <h1 className="title">Memory Game</h1>
-        <p>Click each card only once! if you click the same card you lose!</p>
-        <p className="score">Score: {props.score}</p>
-        <p className="bestScore">Best Score: {props.bestScore}</p>
-      </nav>
-    )
-}
+const Header = (props) => {
+  return (
+    <nav className="navContainer">
+      <h1 className="title">Memory Game</h1>
+      <p className="rules">
+        Click each card only once! <br /> if you click the same <br/> 
+        card twice you lose!
+      </p>
+      <p className="score">Score: {props.score}</p>
+      <p className="bestScore">Best Score: {props.bestScore}</p>
+    </nav>
+  );
+};
 
 export default Header;
